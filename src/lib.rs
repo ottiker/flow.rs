0c08ab4b325b9ebc30606f0b2b9b373154b1b76d
Adrian Ottiker <adrian@ottiker.com>
2017-04-03 20:39:07 +0200

    try to call adapter method

--- src/lib.rs
+++ src/lib.rs
@@ -1,36 +1,53 @@
 
 mod event;
-mod sequence;
+pub mod sequence;
 mod emit;
 
 use event::Event;
+use sequence::{Sequence, Handler, State};
 
-fn emit <'a, A>(adapter: &'static A, sequence_id: &'a String, role: &'a String) -> Event<'a> {
-
-	/* js code:
-		const event = Event(call, options, done);
-
-        Sequence(adapter, call, event, data)
-        .then(Emit)
-        .then(event.open)
-        .catch(event.done);
-
-        return event;
-	*/
-
-	Event {
-		sequence: sequence_id,
-		role: role,
-	}
+pub struct Adapter {
+	_lru_size: usize
+}
+pub trait TAdapter {
+	fn set<'a, T>(key: &str, val: &'a T) -> &'a T;
+	fn get<'a, TAdapter>(adapter: &'static Adapter, key: &str);
+	fn del<'a>(key: &str);
+	fn seq(key: &str);
+	fn fnc(key: &str);
 }
 
-pub fn flow <'a, T>(adapter: &'static T) -> Box<Fn(&'a String, &'a String) -> Event<'a>> {
+pub fn Flow<'a, T: TAdapter>(adapter: &'static Adapter) -> Box<Fn(&'a str, &'a str) -> Event<'a>> {
 
 	/* js code:
 	if (!adapter.cache || !adapter.seq || !adapter.fn) {
 		throw new Error("Flow: Invalid adapter.");
 	}
 	*/
+	// TODO how to make adapter available in the closure?
 
 	Box::new(move |sequence_id, role| emit(adapter, sequence_id, role))
+}
+
+fn emit <'a>(adapter: &'static Adapter, sequence_id: &'a str, role: &'a str) -> Event<'a> {
+
+	/* js code:
+		const event = Event(call, options, done);
+
+		Sequence(adapter, call, event, data)
+		.then(Emit)
+		.then(event.open)
+		.catch(event.done);
+
+		return event;
+	*/
+
+	// TODO how to get the reference to the adapter
+	<Adapter as TAdapter>::get(&adapter, sequence_id);
+
+
+	Event {
+		sequence: sequence_id,
+		role: role,
+	}
 }
